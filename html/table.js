756aa8c24462c5f9cea1051fe37d2c7dcdea2153
Mustapha Nezzari <mnezzari@gmail.com>
2018-06-13 22:59:53 +0200

    Added a resize bar

--- html/table.js
+++ html/table.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var rowCount = 18;
-    $('#left-table tr').each(function(e) {
+    $('#left-table tr').each(function (e) {
         rowCount += $(this).height();
     });
     $('#scrollbar').css({ 'height': rowCount });
     var eventTriggerer;
 
-    var scrollFunction = function(event) {
+    var scrollFunction = function (event) {
         if (eventTriggerer && eventTriggerer !== "scroll-content") {
             eventTriggerer = "";
             return false;
@@ -27,15 +27,15 @@ $(document).ready(function() {
     $('#left-table tbody').on('scroll', scrollFunction);
     $('#right-table tbody').on('scroll', scrollFunction);
 
-    $('#left-table tbody').on('mouseenter', function(e) {
+    $('#left-table tbody').on('mouseenter', function (e) {
         $('#left-table tbody').on('scroll', scrollFunction);
     });
 
-    $('#right-table tbody').on('mouseenter', function(e) {
+    $('#right-table tbody').on('mouseenter', function (e) {
         $('#right-table tbody').on('scroll', scrollFunction);
     });
 
-    $('#scroll-container').on('scroll', function(e) {
+    $('#scroll-container').on('scroll', function (e) {
         if (eventTriggerer && eventTriggerer !== "scrollbar") {
             eventTriggerer = "";
             return false;
@@ -58,7 +58,7 @@ $(document).ready(function() {
     var mouseStart = -1;
     var mouseEnd = -1;
     var id = -1;
-    $('.resize-border').closest('th').each(function(i, e) {
+    $('.resize-border').closest('th').each(function (i, e) {
         debugger;
         var $e = $(e);
         var $resizeBar = $e.find('.resize-border');
@@ -69,27 +69,30 @@ $(document).ready(function() {
     });
 
     $('.resize-border').draggable({
-        start: function(e) {
+        start: function (e) {
             debugger;
             var target = $(e.target);
             id = target.attr('data-id');
             mouseStart = $(this).offset().left;
+            $('.resize-vertical-bar').show();
         },
 
-        drag: function(e) {
-
+        drag: function (e) {
+            $('.resize-vertical-bar').css('left', $(this).offset().left);
         },
 
-        stop: function(e) {
+        stop: function (e) {
             debugger;
             mouseEnd = $(this).offset().left
             var diff = mouseEnd - mouseStart;
             headers[id].width = headers[id].width + diff;
-            headers[id].width = headers[id].width < 0 ? 100 : headers[id].width;
+            headers[id].width = headers[id].width < 100 ? 100 : headers[id].width;
             var $target = $(e.target);
-            $target.closest('th').css('width', headers[id].width);
+            $target.closest('th').width(headers[id].width);
+            $target.closest('table').find('tr td:nth-child(' + id + ')').width(headers[id].width);
 
             // reinit vars
+            $('.resize-vertical-bar').hide();
         }
     })
 });
